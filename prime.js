440469791a344e14d648025184d605680451988c
Mazhar iqbal siddiquee <mazhariqbal963169@gmail.com>
2023-01-12 08:56:27 +0530

    prime

---
+++ prime.js
@@ -0,0 +1,18 @@
+n=7
+let count=0
+ for(let i=2;i<=n;i++)
+{
+  if(n%i==0)
+  {
+  count++;
+  } 
+}
+ if(count==2)
+ {
+  console.log("it is a prime no");
+  }
+  else
+   {
+    console.log("it is not a prime no");
+   }
+  
